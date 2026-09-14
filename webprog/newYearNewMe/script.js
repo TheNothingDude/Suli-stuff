4fd11a0875ef7af40bd86018fc48f3421a71b284
Áron Nemes-Hertelendy <nemes.aron@BOLYAI.LOCAL>
2026-09-14 11:25:35 +0200

    webprogi

--- webprog/newYearNewMe/script.js
+++ webprog/newYearNewMe/script.js
@@ -1,5 +1,6 @@
 class Char {
   #isAlive;
+  items = [];
   static pi = 3.14;
   constructor(name, hp, attack) {
     this.name = name;
@@ -16,10 +17,33 @@ class Char {
     }
     return this.hp;
   }
+  addItem(item) {
+    if (this.items.length <= 5) {
+      this.items.push(item);
+    } else {
+      console.log(`ur inv is full, cant pick up ${item}`);
+    }
+  }
+  removeItem(item) {
+    if ((this.items.length = 0)) {
+      console.log("u have nothing");
+    } else {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
+  showInventory() {
+    console.log(this.items);
+  }
+  heal(x) {
+    this.hp += x;
+  }
 }
 const c = new Char("Sharon", 1, 0.1);
 const c1 = new Char("Orchid", Infinity, Infinity);
 
 console.log(c.hp);
 console.log(c.takeDamage(5));
-console.log(Char.pi);
+console.log(c.items);
+c.addItem("apple");
+console.log(c.items);
+c.removeItem("apple");
