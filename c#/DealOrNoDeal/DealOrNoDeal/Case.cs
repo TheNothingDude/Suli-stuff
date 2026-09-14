a40a78a9e430148a474ce5c8233c4f64dca4410f
nemes.aron <nemes.aron@T201-2>
2026-09-14 14:28:59 +0200

    fixed minor bug

---
+++ c#/DealOrNoDeal/DealOrNoDeal/Case.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _11C_All_Az_Alku
+{
+    internal class Taska
+    {
+        int osszeg;
+        bool nyitva;
+        int sorszam;
+        public int Sorszam { get { return sorszam; } }
+        public bool Nyitva { get { return nyitva; } }
+
+        static int jatekosTaska;
+
+        public Taska(int osszeg, int sorszam)
+        {
+            this.osszeg = osszeg;
+            this.sorszam = sorszam;
+            nyitva = false;
+        }
+
+
+        public static void JatekosTaskaBeallit(int sorszam)
+        {
+            jatekosTaska = sorszam;
+        }
+
+        public static int JatekosTaska
+        {
+            get { return jatekosTaska; }
+        }
+
+        public int Kinyit()
+        {
+            nyitva = true;
+            return osszeg;
+        }
+
+        public int Osszeg()
+        {
+            return osszeg;
+        }
+
+
+        static char[,] taskaRajz = new char[,]
+        {
+            {'┌', '█', '█', '┐' },
+            {'│', ' ', ' ', '│' },
+            {'└', '─', '─', '┘' },
+        };
+
+        public static void TaskaKirajzol(int x, int y, int sorszam)
+        {
+            for (int i = 0; i < taskaRajz.GetLength(0); i++)
+            {
+                for (int j = 0; j < taskaRajz.GetLength(1); j++)
+                {
+
+
+                    Console.SetCursorPosition(x + j, y + i);
+                    Console.Write(taskaRajz[i, j]);
+                }
+            }
+
+            Console.SetCursorPosition(x + 1, y + 1);
+            Console.Write(sorszam.ToString().PadLeft(2, ' '));
+        }
+    }
+}
