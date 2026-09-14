cfcd2cec3ed7c5f27cc4daddfd4e8b97f831472f
Áron Nemes-Hertelendy <nemes.aron@BOLYAI.LOCAL>
2026-09-14 12:00:33 +0200

    webprogi1

--- webprog/newYearNewMe/script.js
+++ webprog/newYearNewMe/script.js
@@ -1,19 +1,20 @@
 class Char {
-  #isAlive;
+  isAlive;
   items = [];
   static pi = 3.14;
-  constructor(name, hp, attack) {
+  constructor(name, hp, attack, ac) {
     this.name = name;
     this.hp = hp;
+    this.ac = ac;
     this.attack = attack;
-    this.#isAlive = true;
+    this.isAlive = true;
   }
   takeDamage(damage) {
     this.hp = this.hp - damage;
     if (this.hp < 0) {
-      this.#isAlive = false;
+      this.isAlive = false;
       this.hp = 0;
-      console.log("he dood");
+      console.log(`${this.name} dood`);
     }
     return this.hp;
   }
@@ -25,25 +26,68 @@ class Char {
     }
   }
   removeItem(item) {
-    if ((this.items.length = 0)) {
-      console.log("u have nothing");
+    if (this.items.indexOf(item) == -1) {
+      console.log("no such item exists");
+      return;
     } else {
-      this.items.splice(this.items.indexOf(item), 1);
+      if ((this.items.length = 0)) {
+        console.log("u have nothing");
+        return;
+      } else {
+        this.items.splice(this.items.indexOf(item), 1);
+      }
     }
   }
   showInventory() {
-    console.log(this.items);
+    return this.items.join(", ");
   }
   heal(x) {
     this.hp += x;
   }
 }
-const c = new Char("Sharon", 1, 0.1);
-const c1 = new Char("Orchid", Infinity, Infinity);
+class Arena {
+  static Fight(c1, c2) {
+    console.log("a");
+    while (c1.isAlive && c2.isAlive) {
+      let initive1 = getRandom(1, 10);
+      let initive2 = getRandom(1, 10);
+      let attackroll1 = getRandom(1, 20);
+      let attackroll2 = getRandom(1, 20);
+      let attack1 = attackroll1 + c1.attack;
+      let attack2 = attackroll2 + c2.attack;
+      if (initive1 > initive2) {
+        if (attack1 > c2.ac) {
+          console.log(`${c1.name} hits`);
+          c2.takeDamage(attack1);
+        } else {
+          console.log(`${c1} misses`);
+        }
+      }
+      if (initive2 > initive1) {
+        if (attack2 > c1.ac) {
+          console.log(`${c2.name} hits`);
+          c1.takeDamage(attack1);
+        } else {
+          console.log(`${c2} misses`);
+        }
+      }
+      if (initive1 == initive2) {
+        initive1 = getRandom(1, 10);
+        initive2 = getRandom(1, 10);
+      }
+    }
+  }
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const c = new Char("Sharon", 1, 0.1, 1);
+const c1 = new Char("Orchid", Infinity, Infinity, Infinity);
 
 console.log(c.hp);
-console.log(c.takeDamage(5));
 console.log(c.items);
 c.addItem("apple");
-console.log(c.items);
-c.removeItem("apple");
+c.addItem("book");
+console.log(c.showInventory());
+Arena.Fight(c, c1);
