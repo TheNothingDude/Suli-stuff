5f0ebcc179a10b009d54152bccc87e22f5f1837e
nemes.aron <nemes.aron@T201-2>
2026-09-21 11:48:53 +0200

    update

--- webprog/konyvecske/src/main.ts
+++ webprog/konyvecske/src/main.ts
@@ -1,5 +1,4 @@
 class Book {
-  // Define types for private fields
   #pagesRead: number = 0;
   #isRead: boolean;
   #title: string;
@@ -10,9 +9,10 @@ class Book {
     this.#title = title;
     this.#author = author;
     this.#page = 1;
-
-    if (Number.isInteger(page) && page > 0) {
+    if (page > 0) {
       this.#page = page;
+    } else {
+      console.log("Not a valid num");
     }
     this.#isRead = isRead;
   }
@@ -20,9 +20,15 @@ class Book {
   get page(): number {
     return this.#page;
   }
+  get title(): string {
+    return this.#title;
+  }
+  get isRead(): boolean {
+    return this.#isRead;
+  }
 
   read(): void {
-    this.#isRead = true; // Fixed: added '#'
+    this.#isRead = true;
     console.log(`You have read the book ${this.#title}`);
   }
 
@@ -32,23 +38,58 @@ class Book {
     );
   }
 
-  pageTurn(): void {
+  pageTurn(x: number): void {
     if (!this.#isRead) {
-      // Fixed: added '#'
-      this.#pagesRead += 1; // Fixed: added '#'
-      console.log(`Page turned! You're now on page ${this.#pagesRead}`);
+      if (x > 0) {
+        this.#pagesRead += x;
+        console.log(`Page turned! You're now on page ${this.#pagesRead}`);
 
-      if (this.#pagesRead === this.#page) {
-        // Fixed: added '#' and used strict equality
-        this.#isRead = true;
+        if (this.#pagesRead === this.#page) {
+          this.#isRead = true;
+        }
+        return;
       }
-      return;
     }
     console.log("book already read");
   }
 }
 
+class Library {
+  #books: Book[];
+  constructor() {
+    this.#books = [];
+  }
+  addBook(book: Book): void {
+    this.#books.push(book);
+  }
+  removeBook(title): void {
+    this.#books.splice(this.#books.map((e) => e.title).indexOf(title, 1));
+  }
+  getUnreadBooks(): Book[] {
+    let unRead: Book[] = [];
+    this.#books.forEach((e) => {
+      if (e.isRead == false) {
+        unRead.push(e);
+      }
+    });
+    return unRead;
+  }
+  get Books(): Book[] {
+    return this.#books;
+  }
+}
+
 const b = new Book("The legend of the kittydom", "Helium", 67, false);
+const b2 = new Book("Appe", "The Apple Man", 10, false);
+const b3 = new Book("smth", "alma", 15, false);
+const l = new Library();
+
 b.getInfo();
-b.pageTurn();
+b.pageTurn(1);
 console.log(b.page);
+
+l.addBook(b);
+l.addBook(b2);
+l.removeBook("Appe");
+console.log(l.Books);
+console.log(l.getUnreadBooks());
